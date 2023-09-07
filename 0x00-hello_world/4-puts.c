f0950764494d45109ff9591e90d767ae67b6fb48
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-07 11:54:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 int main(void)
 {
 	/**
-	 * main the entry point integer a value
-	 * This code writing sting "Programming is
-	 * like building a ltilingual puzzle" 
-	 * to the console
-	 */
+	* function main returuns integer a value without argument
+	* This code writing sting "Programming is
+	* like building a ltilingual puzzle"
+	* to the console
+	*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
