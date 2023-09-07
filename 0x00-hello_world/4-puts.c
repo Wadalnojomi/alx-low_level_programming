1bc3313eafb4b5604f7691a4e6e100f68d7126ba
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-07 11:40:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main(void)
-{ 
-	/* This code writing sting "Programming is like building a ltilingual puzzle" to the console                            */
-	puts("\"Programming is like building a multilingual puzzle\"");
+{
+	/**
+	 * main the entry point integer a value
+	 * This code writing sting "Programming is
+	 * like building a ltilingual puzzle" 
+	 * to the console
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
