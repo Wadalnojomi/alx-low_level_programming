a913051b8f017cfa8254f042c300eb4f9938920a
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-07 14:20:26 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 printf("size of char: %zu byte(s)\n",sizeof (char));
 printf("size of int: %zu byte(s)\n",sizeof (int));
 printf("size of long int: %zu byte(s)\n",sizeof (long int));
-prinyf("size o long long int:%zu byte(s)\n",sizeof (long long int));
+printf("size o long long int:%zu byte(s)\n",sizeof (long long int));
 printf("size of float: %zu byte(s)\n",sizeof(float));
 
 return (0);
