3a54bd19cb7eba5d8af7273bfcefc834ca6fd9b1
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-07 10:38:29 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+int main(void)
+{ 
+	/* This code writing sting "Programming is like building a multilingual puzzle" to the console
+	 */
+  	puts("\"Programming is like building a multilingual puzzle");
+        puts("fllowed by a new line.\"");        
+	return (0);
+}	
