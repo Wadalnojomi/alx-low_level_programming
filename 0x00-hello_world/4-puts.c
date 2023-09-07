a5ecc332b088447d9c665fe988e73775b66af392
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-07 12:36:40 -0700

    pits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-/**
-* main - entry point
-* This code writing sting "Programming is
-* like building a ltilingual puzzle"
-* to the console
-*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
