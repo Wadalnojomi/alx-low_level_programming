7d102cc41e8a8f8fddc44241ec5fe80dd31d66cb
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-07 14:12:19 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+printf("size of char: %zu byte(s)\n",sizeof (char));
+printf("size of int: %zu byte(s)\n",sizeof (int));
+printf("size of long int: %zu byte(s)\n",sizeof (long int));
+prinyf("size o long long int:%zu byte(s)\n",sizeof (long long int));
+printf("size of float: %zu byte(s)\n",sizeof(float));
+
+return (0);
+}
