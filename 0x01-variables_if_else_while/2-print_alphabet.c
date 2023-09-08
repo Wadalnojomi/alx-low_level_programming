92744693f430ee7f600b84133fe6ae84f0a1d3f3
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-08 11:44:01 -0700

    a ro z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,14 @@
 /**
  * main - prints the alphabet in lowercase
  * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
+	putchar(c);
 	}
 	putchar('\n');
 	return (0);
