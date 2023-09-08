e0887e836e96472e46f36c4212c82a23f495d73b
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-08 15:53:16 -0700

    e q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet except e and q
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
+
