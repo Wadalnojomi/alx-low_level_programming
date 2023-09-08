584fe86b27b62f4377ba4007ceed5c82d556ddc1
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-08 16:51:00 -0700

    numerz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers from 0 to 9
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+	putchar(x + '0');
+	}
+	putchar('\n');
+	return (0);
+}
