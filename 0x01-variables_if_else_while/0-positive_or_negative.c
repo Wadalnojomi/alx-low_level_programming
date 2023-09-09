df95c2146ae526eb001b55ad25235b6aa41b0aef
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-09 04:50:57 -0700

    +or-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,18 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand()  - RAND_MAX / 2;
-
+	printf("%d \n", n);
 	if (n > '0')
 	{
-	printf("%d is positive\n", n);
+	printf("is positive");
 	}
 	else if (n == '0')
 	{
-	printf("%d is zero\n", n);
+	printf("is zero");
 	}
 	else
 	{
-	printf("%d is negative\n");
+	printf("is negative");
 	}
 	return (0);
 }
