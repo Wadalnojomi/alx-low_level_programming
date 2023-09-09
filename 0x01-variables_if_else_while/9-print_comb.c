2fa0318531607c1f791d6d972398ef1edc86fa5b
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-08 18:57:41 -0700

    gs

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,13 +4,15 @@
  * main - Prits numbers separate by coma
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int y;
 	for (y = '0'; y <= '9'; y++)
+
 	{
 	putchar(y);
-	if (y != 9)
+	if (y != '9')
 	{
 	putchar(',');
 	putchar(' ');
