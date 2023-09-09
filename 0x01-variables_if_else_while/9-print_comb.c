01eb2845c2610b5038f87af64eaec823cf75ff2a
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-08 18:48:08 -0700

    xomb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prits numbers separate by coma
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int y;
+	for (y = '0'; y <= '9'; y++)
+	{
+	putchar(y);
+	if (y != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
