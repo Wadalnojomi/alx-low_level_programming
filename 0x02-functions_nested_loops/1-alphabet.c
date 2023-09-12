fb13e2f9b0ccc21ef5ddc8b6ca892ab9dfafb860
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-12 10:42:34 -0700

    atoz

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+}
