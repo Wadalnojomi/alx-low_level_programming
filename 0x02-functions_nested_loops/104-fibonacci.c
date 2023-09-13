abf6dbbd853d514b4db64e0941a1f492cc6b2f0f
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-12 18:34:19 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,11 +3,12 @@
  * main - finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2
  * followed by a new line
- * Return: ALways 0 (Success) 
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
+
 	j = 1;
 	k = 2;
 
