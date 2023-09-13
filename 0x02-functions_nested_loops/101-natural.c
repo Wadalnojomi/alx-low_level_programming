55506af61a43f2963161c9daa2b222012f59099d
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-12 17:52:11 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,24 +7,17 @@
  */
 int main(void)
 {
-	long unsigned int sum3, sum5, sum;
-	int i;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-	for (i =0; i<=1024; i++)
+	int i, z = 0;
+
+	while (i < 1024)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3 == 0) || (i %5 == 0))
 		{
-			sum3 = sum3 + 1;
+			z += i;
 		}
-		else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + 1;
-		}
+		i++;
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
+	printf("%d\n", z);
 	return (0);
 }		
