12be556647fb5c24575966507153181a465151d8
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-14 06:49:04 -0700

    print num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers "0" to "9", except 2 and 4
+ *
+ * Return: is zero
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x < 10; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
