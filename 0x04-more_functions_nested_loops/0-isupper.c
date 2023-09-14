a937e8ae1821d10616ffcf7884d8b2cd4a6b6fb7
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-14 05:08:04 -0700

    isuppr

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-  * _isdigit - If is 0 to 9 return 1, if not return 0, function check a digit
-  *
-  * @c: this is the entry
-  *
-  * Return: 0
-  *
-  **/
-int _putchar(int c)
+ * _isupper - If is uppercase return 1, if not return 0
+ *
+ * @c: this is the entry
+ *
+ * Return: 0
+ *
+ **/
+int _isupper(int c)
 
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
