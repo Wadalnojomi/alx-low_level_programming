dd36d583779cbf59cd68ce8404dd15ebe84e38b8
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-18 03:44:38 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value it points to as 98.
+ * @n: this is the entry
+ *
+ * Return: Always (0)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
