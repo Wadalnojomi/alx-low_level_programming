4f66f81ad184f08ad26c7923079436f46e4ab2a1
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-28 07:55:44 -0700

    sd

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int my_sqrt(int n, int g)
 int is_prime_number(int n);
 
 #endif
