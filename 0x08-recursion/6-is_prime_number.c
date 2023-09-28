4c6c623160f7591f05b364603e25b72a32ea329d
wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-09-28 12:44:51 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * is_prime_number - returns 1 if the input integer is a prime num
- * otherwise return 0.
- * is_prime_helper - Reduced the range of numbers checked for divisibility
- * @n: Is the number to be cheack.
+ * is_prime_helper - function to improve efficiency
  * @current_divisor: to check the prime numder
+ * @n: Is the number to be cheac
  * Return: if the input integer is a prime number, otherwise return 0
  */
 int is_prime_helper(int n, int current_divisor)
@@ -16,6 +14,12 @@ int is_prime_helper(int n, int current_divisor)
 	else
 		return (is_prime_helper(n, current_divisor - 1));
 }
+/**
+ * is_prime_number - returns 1 if the input integer is a prime num
+ * otherwise return 0.
+ * @n: Is the number to be cheac
+ * Return: if the input integer is a prime number, otherwise return 0
+ */
 int is_prime_number(int n)
 {
 	if (n < 2)
