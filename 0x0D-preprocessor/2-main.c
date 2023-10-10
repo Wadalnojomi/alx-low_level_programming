32dcf912ff266d9e145b1c64e93a73d1fb1b9777
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-10 09:39:36 -0700

    __file__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Write program that prints the name of file it compiled from.
+ *
+ * Return: 0 in success.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
