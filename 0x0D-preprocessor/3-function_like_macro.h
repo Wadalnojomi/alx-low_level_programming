5c7cdc52f557537e45961116b6b668f6ae590862
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-10 09:55:02 -0700

    sum

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef MACRO_H
-#define MACRO_H
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef MACRO_FUNCTION_LIKE
+#define MACRO_FUNCTION_LIKE
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
