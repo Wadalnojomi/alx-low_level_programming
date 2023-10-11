c2060278d86f70b0272f68f3c2b6691805a3f521
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-11 10:35:06 -0700

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name -  prints a name.
+ * @name: name to be writen.
+ * @f: function pointer to string.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name)
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void print_name(char *name, void (*f)(char *);
+int _putchar(char c);
+
+
+#endif
