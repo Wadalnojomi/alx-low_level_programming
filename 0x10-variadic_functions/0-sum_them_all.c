759184bf268a25cbd88edfaa91d2b637ff8f296f
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-12 03:50:56 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,18 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-	va_list args;
+	va_list numbers;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_start(args, n);
+	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, int);
+		sum += va_arg(numbers, int);
 	}
-	va_end(args);
+	va_end(numbers);
 
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,9 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VATIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VATIADIC_FUNCTIONS_H
 #include <stdarg.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
 
