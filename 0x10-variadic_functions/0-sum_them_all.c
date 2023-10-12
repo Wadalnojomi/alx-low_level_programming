a20c07cbe81edabefc71bf5b51bc35235e3eb627
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-12 04:21:31 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
+	va_list nums;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list nums;
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
 	{
