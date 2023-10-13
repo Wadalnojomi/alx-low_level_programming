03f81468470de93bbd1121a68605dda4898af64e
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-12 23:48:14 -0700

    all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,9 +32,11 @@ void print_all(const char * const format, ...)
 			case 's':
 				s = va_arg(args, char *);
 				if (s == NULL)
+				{
 					printf("nil");
-				else
-					printf("%s", s);
+					return;
+				}
+				printf("%s", s);
 				break;
 			default:
 				break;
