d716e8e68eced86168d84f5e3a5ecba45a194a15
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-24 04:44:20 -0700

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list.
+ * Return: nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+		temp = NULL;
+	}
+}
