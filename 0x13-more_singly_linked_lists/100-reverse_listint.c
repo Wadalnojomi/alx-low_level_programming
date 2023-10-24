32ddcc56d8fbcc19bca3b1ff36680ce3700b9ad2
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-24 12:09:53 -0700

    rev

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,7 +2,7 @@
 /**
  * reverse_listint -  reverses a listint_t linked list.
  * @head: list.
- * Returns: a pointer to the first node of the reversed list.
+ * Return: a pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
@@ -17,5 +17,5 @@ listint_t *reverse_listint(listint_t **head)
 		*head = next;
 	}
 	*head = prev;
-	return(prev);
+	return (prev);
 }
