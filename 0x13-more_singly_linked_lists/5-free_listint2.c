e5fa5dc47d0dc6fe8939e5f3cdd43770fbddaffd
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-24 05:34:00 -0700

    free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return;
 	}
@@ -17,7 +17,6 @@ void free_listint2(listint_t **head)
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-		temp = NULL;
 	}
 	*head = NULL;
 }
