b208b57b25ca72aabd503a80815600bcb5027fdc
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-30 18:56:12 -0700

    reate

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,27 @@
+#include "main.h"
 /**
  * create_file - creates a file
  * @filename: file to be created
  * @text_content: Null terminated string to be written to the file
  * Return: -1 on failure 1 on success
  */
-
 int create_file(const char *filename, char *text_content)
 {
-        int fd, nlet, rwr;
+	int fd, n, r;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	for (n = 0; text_content[n]; n++)
+		;
+	r = write(fd, text_content, n);
+	if (r == -1)
+		return (-1);
+	close(fd);
 
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-        if (fd == -1)
-                return (-1);
-        if (text_content == NULL)
-                text_content = "";
-        for (nlet = 0; text_content[nlet]; nlet++)
-                ;
-        rwr = write(fd, text_content, nlet);
-        if (rwr == -1)
-                return (-1);
-        close(fd);
-        return (1);
+	return (1);
 }
